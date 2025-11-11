5389f9ab9cc256d1533939a124c66c3df1b75cb2
raju080808 <135940031+raju080808@users.noreply.github.com>
2025-11-11 19:24:09 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,9 @@ import companyRouter from './routes/company.routes'
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(  cors({
+    origin: "*",
+  }));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
